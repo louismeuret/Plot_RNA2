9e02033e22eb4361260c938004b26801b1b62a2b
Louis Meuret <louis23012001@gmail.com>
2025-05-30 11:39:07 +0200

    Update start_services.sh

--- start_services.sh
+++ start_services.sh
@@ -7,7 +7,7 @@ nohup celery -A tasks_celery worker --loglevel=INFO --concurrency=10 > celery_wo
 nohup celery --broker=redis://localhost:6379/0 flower > flower_output.log 2>&1 &
 
 # Start Gunicorn with eventlet and store output
-nohup gunicorn --worker-class eventlet --access-logfile '-' --error-logfile '-' --timeout 600 -b 127.0.0.1:6969 -w 1 'app7:app' > gunicorn_output.log 2>&1 &
+nohup gunicorn --worker-class eventlet --access-logfile '-' --error-logfile '-' --timeout 600 -b 127.0.0.1:4242 -w 1 'app7:app' > gunicorn_output.log 2>&1 &
 
 echo "All services are started in the background."
 
